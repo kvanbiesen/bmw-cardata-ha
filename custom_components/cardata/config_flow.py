kvanbiesen/bmw-cardata-ha

commit 1e1bb6b84f2460f32d4451f4e5ac4eedbc8a7654

CI tweaks and mypy fixes

custom_components/cardata/config_flow.py
@@ -17,6 +17,7 @@ from homeassistant.components import persistent_notification
 from homeassistant.core import callback
 from homeassistant.data_entry_flow import FlowResult, FlowResultType
 
+from .const import DOMAIN
 _LOGGER = logging.getLogger(__name__)
 
 # Maximum length for error messages shown to users
@@ -76,7 +77,8 @@ def _generate_code_challenge(code_verifier: str) -> str:
 
 
 # type: ignore[call-arg]
-class CardataConfigFlow(config_entries.ConfigFlow, domain="cardata"):
+class CardataConfigFlow(config_entries.ConfigFlow):
+    domain = DOMAIN
     """Handle config flow for BMW CarData."""
 
     VERSION = 1

.github/workflows/ci.yml
@@ -28,7 +28,7 @@ jobs:
         run: flake8 --ignore=E501,W503,E203 custom_components/cardata
 
       - name: Run mypy (ignore missing imports)
-        run: mypy --ignore-missing-imports custom_components/cardata
+        run: mypy --namespace-packages --explicit-package-bases --ignore-missing-imports custom_components/cardata
 
       - name: Compile all
         run: python -m compileall custom_components/cardata

custom_components/cardata/stream.py
@@ -8,7 +8,7 @@ import logging
 import ssl
 import time
 from enum import Enum
-from typing import Awaitable, Callable, Optional
+from typing import Awaitable, Callable, Optional, Coroutine, Any, cast
 
 import paho.mqtt.client as mqtt
 
@@ -339,7 +339,7 @@ class CardataStreamManager:
             if self._status_callback:
                 asyncio.run_coroutine_threadsafe(
                     # type: ignore[arg-type]
-                    self._status_callback("connected", None),
+                    cast(Coroutine[Any, Any, None], self._status_callback('connected', None)),
                     self.hass.loop,
                 )
         elif rc in (4, 5):  # bad credentials / not authorized
@@ -371,7 +371,7 @@ class CardataStreamManager:
             if self._status_callback:
                 asyncio.run_coroutine_threadsafe(
                     # type: ignore[arg-type]
-                    self._status_callback("connection_failed", str(rc)),
+                    cast(Coroutine[Any, Any, None], self._status_callback('connection_failed', str(rc))),
                     self.hass.loop,
                 )
 
@@ -396,7 +396,7 @@ class CardataStreamManager:
             return
         asyncio.run_coroutine_threadsafe(
             # type: ignore[arg-type]
-            self._message_callback(data), self.hass.loop
+            cast(Coroutine[Any, Any, None], self._message_callback(data)), self.hass.loop
         )
 
     def _handle_disconnect(self, client: mqtt.Client, userdata, rc) -> None:
@@ -459,7 +459,7 @@ class CardataStreamManager:
             if self._status_callback:
                 asyncio.run_coroutine_threadsafe(
                     # type: ignore[arg-type]
-                    self._status_callback("unauthorized", reason),
+                    cast(Coroutine[Any, Any, None], self._status_callback('unauthorized', reason)),
                     self.hass.loop,
                 )
         else:
@@ -469,7 +469,7 @@ class CardataStreamManager:
             if self._status_callback:
                 asyncio.run_coroutine_threadsafe(
                     # type: ignore[arg-type]
-                    self._status_callback("disconnected", reason),
+                    cast(Coroutine[Any, Any, None], self._status_callback('disconnected', reason)),
                     self.hass.loop,
                 )
 

commit bd0df78ff1e8deea3b113f3c4ce74645bb25927d

Fix the ConfigFlow regression introduced by 1e1bb6b84f2460f32d4451f4e5ac4eedbc8a7654

custom_components/cardata/config_flow.py
@@ -77,8 +77,7 @@ def _generate_code_challenge(code_verifier: str) -> str:
 
 
 # type: ignore[call-arg]
-class CardataConfigFlow(config_entries.ConfigFlow):
-    domain = DOMAIN
+class CardataConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle config flow for BMW CarData."""
 
     VERSION = 1