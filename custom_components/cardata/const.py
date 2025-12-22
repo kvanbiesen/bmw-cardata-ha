fc651f2ef94709596edd9bdfcc7e98323d6d8a65
kvanbiesen <kvanbiesen@gmail.com>
2025-12-22 15:20:27 +0100

    Remove vehicle.isMoving from constants
    Removed 'vehicle.isMoving' from the constants list.

--- custom_components/cardata/const.py
+++ custom_components/cardata/const.py
@@ -93,7 +93,6 @@ HV_BATTERY_DESCRIPTORS = [
     "vehicle.drivetrain.electricEngine.charging.level",
     "vehicle.powertrain.electric.battery.stateOfHealth.displayed",
     "vehicle.vehicleIdentification.basicVehicleData",
-    "vehicle.isMoving",
     "vehicle.drivetrain.batteryManagement.batterySizeMax",
     "vehicle.drivetrain.batteryManagement.maxEnergy",
     "vehicle.powertrain.electric.battery.charging.power",
